5630fc0a0400fcb66aed4f3fe24e8c3e0ad85483
CoKoala <ali@capsai.org>
2023-09-16 06:19:49 +0000

    feat: longest common prefixes
    Understanding loops and slices in go

---
+++ compare_strings/compare_strings.go
@@ -0,0 +1,22 @@
+package comparestrings
+
+func CompareStrings(strs []string) string {
+
+	if strs == nil {
+		return ""
+	}
+
+	prefix := strs[0]
+	for _, str := range strs[1:] {
+		i := 0
+		for i < len(prefix) && i < len(str) && str[i] == prefix[i] {
+			i++
+		}
+
+		prefix = prefix[:i]
+		if prefix == "" {
+			return ""
+		}
+	}
+	return prefix
+}
